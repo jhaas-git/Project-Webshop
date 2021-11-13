afd38b64e0bd9161456670d68d400be938e7ba0e
JH-Development <joeyhaas.git@gmail.com>
2021-11-13 14:14:36 +0100

    Header
    Building of a responsive navigation.
    
    When the viewport is less than 768 pixels wide a smaller logo and burger menu will be displayed.
    
    If the viewport exceeds 768 pixels, a different logo and quick links will be displayed.
    
    Mobile menu opens with JavaScript and body overflow will be hidden. Content will be implemented later on.

---
+++ template/default/header.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="navigation-mobile" id="menu"></div>
+    <div class="navigation-container">
+        <div class="navigation-content">
+            <div class="navigation-content-left mobile" id="mobile-menu"><span></span></div>
+            <div class="navigation-content-left desktop">
+                <ul class="navigation-content-left-links">
+                    <li><a href="#" class="links">stories</a></li>
+                    <li><a href="#" class="links">heritage</a></li>
+                    <li><a href="#" class="links">watches</a></li>
+                </ul>
+            </div>
+            <div class="navigation-content-center mobile"><a href="#"><img src="../media/brand/logo-black-sm.svg" alt=""></a></div>
+            <div class="navigation-content-center desktop"><a href="#"><img src="../media/brand/logo-black-lg.svg" alt=""></a></div>
+            <div class="navigation-content-right">
+                <ul class="navigation-content-right-links">
+                    <li><a href="#" class="bi bi-bag"></a></li>
+                    <li><a href="#" class="bi bi-person-circle"></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ template/product.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../design/default/header.css">
+    <title>echo modelname | Audemars Piguet</title>
+</head>
+<body>
+    
+<?php include 'default/header.php'; ?>
+
+<main style="min-height: 100vh;">
+
+</main>
+
+<script src="../javascript/menu.js"></script>
+</body>
+</html>
