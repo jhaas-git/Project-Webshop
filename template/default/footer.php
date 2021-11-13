e73cd1dbe1b5e46ea57ee39fe09a99f80e77f060
JH-Development <joeyhaas.git@gmail.com>
2021-11-13 15:13:45 +0100

    Footer
    Building the responsive footer.
    
    Displays the correct logo depending on width just like the header.
    
    When the viewport exceeds 475 pixels, all columns will be displayed in a square. After 768 pixels, all columns will be inline.

---
+++ template/default/footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="footer-container">
+        <div class="footer-column">
+            <ul>
+                <li class="mobile"><a href="#" class="image"><img src="../media/brand/logo-black-sm.svg" alt=""></a></li>
+                <li class="desktop"><a href="#" class="image"><img src="../media/brand/logo-black-lg.svg" alt=""></a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <ul>
+                <li><p class="title">Watches</p></li>
+                <li><a href="#" class="link">collections</a></li>
+                <li><a href="#" class="link">all watches</a></li>
+                <li><a href="#" class="link">stories</a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <ul>
+                <li><p class="title">Services</p></li>
+                <li><a href="#" class="link">our services</a></li>
+                <li><a href="#" class="link">extend warranty</a></li>
+                <li><a href="#" class="link">contact us</a></li>
+                <li><a href="#" class="link">FAQ</a></li>
+            </ul>
+        </div>
+        <div class="footer-column">
+            <ul>
+                <li><p class="title">Company</p></li>
+                <li><a href="#" class="link">foundation</a></li>
+                <li><a href="#" class="link">careers</a></li>
+                <li><a href="#" class="link">press</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
--- template/product.php
+++ template/product.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../design/default/header.css">
+    <link rel="stylesheet" href="../design/default/footer.css">
     <title>echo modelname | Audemars Piguet</title>
 </head>
 <body>
@@ -15,6 +16,8 @@
 <main style="min-height: 100vh;">
 
 </main>
+
+<?php include 'default/footer.php'; ?>
 
 <script src="../javascript/menu.js"></script>
 </body>
