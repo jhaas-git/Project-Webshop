e9d979b4dc99d2e251936289c048208b665454ab
JH-Development <joeyhaas.git@gmail.com>
2021-11-15 14:43:15 +0100

    Watches.php and PDO connection
    Connecting to the database with PDO.
    
    Watches.php:
    Laying out and styling the result is done. Soon the product display will be incorporated into a function with a foreach.
    
    The page will contain a filter functionality aswell. This will be made after completion of fetching results.

---
+++ model/config/db.php
@@ -0,0 +1,6 @@
+<?php 
+     $host = 'docker_mysql_1';
+     $db = 'AudemarsPiguet';
+     $user = 'root';
+     $password = 'root';
+?>
---
+++ template/watches.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../design/default/header.css">
+    <link rel="stylesheet" href="../design/content/watches.css">
+    <link rel="stylesheet" href="../design/default/footer.css">
+    <title>Our watches | Audemars Piguet</title>
+</head>
+<body>
+    
+<?php include 'default/header.php'; ?>
+
+<main>
+
+<section class="watch-results-section">
+    <div class="watch-results-container">
+        <div class="product-display">
+            <a href="#">
+            <div class="product-image"><img src="../media/test/watch.png" alt="product-image"></div>
+            <div class="product-values">
+                <ul>
+                    <li id="model-name">Minute Repeater Supersonnerie</li>
+                    <li id="model-price">74.500,00</li>
+                </ul>
+            </div>
+            </a>
+        </div>
+    </div>
+</section>
+
+</main>
+
+<?php include 'default/footer.php'; ?>
+
+<script src="../javascript/menu.js"></script>
+
+</body>
+</html>
